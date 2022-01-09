c36a03b0a514da14ec0f68570647c59f16d0f727
srukle <srukle@dreamwidth.org>
2022-01-09 14:20:55 -0600

    update: adding mail feature

--- carrier.py
+++ carrier.py
@@ -1,8 +1,10 @@
 import datetime
 from dateutil.parser import parse
+from email.mime.text import MIMEText
 import feedparser
 import os
 import requests
+import smtplib
 import urllib3
 
 def gather_feeds(location):
@@ -32,8 +34,25 @@ def parse_feed(url):
 	except Exception as e:
 		pass
 	for entry in d.entries:
-		if parse(entry.published).strftime('%Y-%m-%d') >= (datetime.datetime.today() - datetime.timedelta(days=3)).strftime('%Y-%m-%d'):
+		if parse(entry.published).strftime('%Y-%m-%d') >= (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d'):
 			write_to_directory('<p><a href=\"' + entry.link + '\">' + entry.title + '</a></p>')
+
+def send_mail(content):
+	configs = []
+	with open ('dw-config.txt', 'rb') as f:
+		for line in f:
+			configs.append(line.rstrip().decode())
+	msg = MIMEText('post-security: public\n' + content)
+	sender = configs[0]
+	recipient = configs[1]
+	msg['Subject'] = 'The Daily Herald - ' + str(datetime.datetime.today().strftime('%Y-%m-%d'))
+	msg['From'] = sender
+	msg['To'] = recipient
+	server = smtplib.SMTP_SSL(configs[2], configs[3])
+	server.login(configs[4], configs[5])
+	server.sendmail(sender, recipient, msg.as_string())
+	server.quit()
+
 
 def write_to_directory(content):
 	directory = "journal_entry"
@@ -56,3 +75,10 @@ feeds = gather_feeds('list.txt')
 
 for url in feeds:
 	parse_feed(url)
+
+
+file_name = datetime.datetime.today().strftime('%Y-%m-%d') + '.entry'
+with open('journal_entry/' + file_name, 'r') as f:
+    output = f.read()
+
+send_mail(output)
